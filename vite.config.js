08e11c8c4f3b320b6cd122d8b822a875e1dbf7e0
OmgbaKevin <kevinomgba1@gmail.com>
2025-06-03 20:21:25 +0100

    removed modification and added option port

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
   plugins: [react(), tailwindcss()],
 })
